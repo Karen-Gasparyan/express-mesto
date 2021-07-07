9ab5f4bec55198ce6d5faaab5fdcfccfee614f0a
Karen Gasparyan <gks181188@yandex.ru>
2021-07-07 16:47:37 +0300

    fixed fix

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,15 +1,13 @@
 const winston = require('winston');
 const expressWinston = require('express-winston');
 
-// создадим логгер запросов
 const requestLogger = expressWinston.logger({
-  transports: [ // то, куда нужно писать лог
+  transports: [
     new winston.transports.File({ filename: 'request.log' }),
   ],
   format: winston.format.json(),
 });
 
-// логгер ошибок
 const errorLogger = expressWinston.errorLogger({
   transports: [
     new winston.transports.File({ filename: 'error.log' }),
@@ -17,7 +15,6 @@ const errorLogger = expressWinston.errorLogger({
   format: winston.format.json(),
 });
 
-// экспортируем в app.js
 module.exports = {
   requestLogger,
   errorLogger,
