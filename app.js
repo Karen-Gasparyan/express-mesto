2e9ad08b7eb2a21740b38cba16f163df6d9a2d34
Karen Gasparyan <gks181188@yandex.ru>
2021-07-09 13:09:12 +0300

    fixed CORS

--- app.js
+++ app.js
@@ -12,6 +12,20 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', `http://localhost:${PORT}`);
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, HEAD, PUT, PATCH, POST, DELETE',
+  );
+
+  next();
+});
+
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
   useNewUrlParser: true,
