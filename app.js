1d9bb51ddd0da6b18e3efa34c067c4673693d9c7
Karen Gasparyan <gks181188@yandex.ru>
2021-07-09 15:14:13 +0300

    fixed CORS-8

--- app.js
+++ app.js
@@ -13,21 +13,22 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const app = express();
 
 app.use((req, res, next) => {
-  const { method } = req;
-  // const requestHeaders = req.headers['access-control-request-headers'];
+  // const { method } = req;
 
   res.header('Access-Control-Allow-Origin', '*');
 
-  if (method === 'OPTIONS') {
-    res.header(
-      'Access-Control-Allow-Methods',
-      'GET, HEAD, PUT, PATCH, POST, DELETE',
-    );
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept',
-    );
-  }
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, HEAD, PUT, PATCH, POST, DELETE',
+  );
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+
+  // if (method === 'OPTIONS') {
+
+  // }
 
   next();
 });
