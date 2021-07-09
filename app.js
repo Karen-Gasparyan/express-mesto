aa8a06f451415f2b2f496d3770410382841d2ae2
Karen Gasparyan <gks181188@yandex.ru>
2021-07-09 16:16:38 +0300

    fixed CORS-10

--- app.js
+++ app.js
@@ -13,20 +13,16 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const app = express();
 
 app.use((req, res, next) => {
-  const { method } = req;
-
   res.header('Access-Control-Allow-Origin', '*');
 
-  if (method === 'OPTIONS') {
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept',
-    );
-    res.header(
-      'Access-Control-Allow-Methods',
-      'GET, HEAD, PUT, PATCH, POST, DELETE',
-    );
-  }
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET,HEAD,PUT,PATCH,POST,DELETE',
+  );
 
   next();
 });
