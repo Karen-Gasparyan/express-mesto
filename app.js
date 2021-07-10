754db44fe47bfc8db8327f85735922543e969042
Karen Gasparyan <gks181188@yandex.ru>
2021-07-10 12:29:38 +0300

    fixed app-2

--- app.js
+++ app.js
@@ -43,12 +43,8 @@ app.post('/signup', celebrate({
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-
-  const { method } = req;
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  }
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
 
   next();
 });
