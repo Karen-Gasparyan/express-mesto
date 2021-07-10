0bae10f5bc0d855c5ac19e9f8f21e47b577ae0d5
Karen Gasparyan <gks181188@yandex.ru>
2021-07-10 12:57:26 +0300

    fixed app-3

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const app = express();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://yp.gks.mesto.nomoredomains.club');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
 
