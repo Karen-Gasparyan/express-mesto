d54e3d061746ae5a931b206dd7d3f27f24c1fdd6
Karen Gasparyan <gks181188@yandex.ru>
2021-07-10 14:11:19 +0300

    fixed app-4

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
 
   next();
