8eebc2c5c76b81597b9453d37ea66de4eca998f6
Karen Gasparyan <gks181188@yandex.ru>
2021-07-11 17:33:46 +0300

    fixed res status 200 send

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { celebrate, Joi, errors } = require('celebrate');
-const cors = require('cors');
+// const cors = require('cors');
 
 const { PORT, IMAGE_REGEX } = require('./config');
 const { login, createUser } = require('./controllers/users');
@@ -31,13 +31,14 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     // res.header('Access-Control-Allow-Credentials', true);
+    res.status(204).send();
     console.log('===>');
   }
 
   next();
 });
 
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
