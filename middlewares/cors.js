84bd07409c4ca28c7f43f956bda832b6e83951cf
Karen Gasparyan <gks181188@yandex.ru>
2021-07-12 13:44:47 +0300

    fixed Headers

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -14,7 +14,6 @@ module.exports = ((req, res, next) => {
     if (req.method === 'OPTIONS') {
       res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
       res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-      res.header('Access-Control-Allow-Credentials', true);
       res.status(204).send();
     }
   }
