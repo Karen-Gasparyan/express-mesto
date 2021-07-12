f2abb3a9b6a9f80c27a42ae1ce4ee8b6ae388a1f
Karen Gasparyan <gks181188@yandex.ru>
2021-07-12 12:39:24 +0300

    fixed port

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -4,7 +4,6 @@ module.exports = ((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   res.header('Access-Control-Allow-Credentials', true);
-  res.status(204).send();
 
   next();
 });
