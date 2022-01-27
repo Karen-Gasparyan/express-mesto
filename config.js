65f05517eacd44bd790ef7336d81693c11446cf6
Karen Gasparyan <gks181188@yandex.ru>
2022-01-27 16:17:51 +0300

    FIXED: default user info

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ const PORT = process.env.PORT || 5000;
 const IMAGE_REGEX = /^(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?$/i;
 
 const DEFAULT_USER = {
-  name: 'Жак-Ив Кусто',
-  about: 'Исследователь',
-  avatar: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+  name: 'Ваше имя',
+  about: 'Ваша специальность',
+  avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzPb_pSj-ir-9eB6mi0lVJdQP1KKHiB8fRBS1CbmOXGd9Z1FEGMJHbEKhahwhWLGSaEXY&usqp=CAU',
 };
 
 const JWT_DEV = 'dev-secret';
